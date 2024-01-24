d2ad073e8b1076cd9f44f227db1a081f0a1c8210
Marco <marco.pasqua03@gmail.com>
2024-01-24 18:22:27 -0500

    Added new style to render cards, created login page file

--- careo/pages/_app.js
+++ careo/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import '../styles/bootstrap.min.css'; //Theme used is cyborg theme from bootswatch.com
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
---
+++ careo/pages/login.js
@@ -0,0 +1,15 @@
+import { Card } from "react-bootstrap"
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import Button from "react-bootstrap";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Login(){
+    return(
+        <>
+            
+            
+        </>
+    )
+}

2300832d019a5bbc86c33ff550bb05ab0e9397dc
Brady Rhora <brady0423@gmail.com>
2024-01-31 17:37:51 -0500

    Added header to all pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
+        <div id="header">
+          <Link href="/"><Image width="32" height="32" src="/assets/careo-temp-logo.png" alt="Careo Logo"></Image></Link>
+        </div>
         <Main />
         <NextScript />
       </body>

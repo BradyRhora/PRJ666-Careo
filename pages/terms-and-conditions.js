33ec1ea0b5fe03b0581fac3a8a27cb81d13dd1fb
Marco <marco.pasqua03@gmail.com>
2024-02-07 14:12:07 -0500

    Place holder text put on created page

---
+++ pages/terms-and-conditions.js
@@ -0,0 +1,16 @@
+import { Card, Form, Alert, Button } from "react-bootstrap"
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import { useState } from 'react';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function termsAndConditions(){
+
+
+    return (
+        <div className='centered'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus in hac habitasse platea dictumst quisque sagittis purus. Senectus et netus et malesuada. Diam vulputate ut pharetra sit amet aliquam id diam. Tellus rutrum tellus pellentesque eu tincidunt. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. In fermentum et sollicitudin ac orci phasellus egestas tellus. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Porta lorem mollis aliquam ut. Urna id volutpat lacus laoreet non curabitur gravida arcu ac. Odio euismod lacinia at quis risus sed. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Tellus mauris a diam maecenas sed enim ut. Magna ac placerat vestibulum lectus mauris. Convallis tellus id interdum velit laoreet id. Purus semper eget duis at tellus at urna condimentum. Tempor orci eu lobortis elementum nibh tellus molestie nunc non. Sodales ut etiam sit amet. In dictum non consectetur a erat nam at.
+        </div>  
+    );
+}

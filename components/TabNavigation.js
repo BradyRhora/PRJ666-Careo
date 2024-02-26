f181770c6b1f76ff75689727efd072afbcfb3970
Aditya Rahman <arahman5395@gmail.com>
2024-02-26 11:12:59 -0500

    user-profile Craeted user-prifle page and added a tab navigation for the core features (recommendation, compatibility, and cart).

---
+++ components/TabNavigation.js
@@ -0,0 +1,20 @@
+import Nav from 'react-bootstrap/Nav';
+
+export default function TabNavigation() {
+    return (
+        <Nav justify variant='tabs' id='tab-nav'>
+            <Nav.Item>
+                <Nav.Link href='/user-profile'>Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href='/recommendation'>Recommendation</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href='/compatibility'>Compatibility</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href='/shopping-cart'>Cart</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
+}

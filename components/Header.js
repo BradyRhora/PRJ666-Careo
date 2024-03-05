8e2590ab897ad74ae1d7134ea254fe5390e14403
Brady Rhora <brady0423@gmail.com>
2024-03-04 20:50:48 -0500

    Updated TabNavigation for mobile, added to header, more progress on recommendation page

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,8 @@ import { useAtom } from "jotai";
 import { userAtom } from "@/store";
 import { useRouter } from "next/router";
 
+import TabNavigation from "@/components/TabNavigation";
+
 
 export default function Header() {
     const [userData, setUserData] = useAtom(userAtom);
@@ -20,9 +22,13 @@ export default function Header() {
     }
         
     return (
-        <div id="header">
-            <Link href="/"><Image width="32" height="32" src="/assets/careo-temp-logo.png" alt="Careo Logo"></Image></Link>
-            {userData && userData.email ? <span>Welcome, <b>{userData.email.split('@')[0]}</b> <a href='' style={{fontSize: '0.8em'}} onClick={handleLogout}>Logout</a></span> : null}
-        </div>
+        <>
+            <div style={{height:"50px"}}className="spacer"></div>
+            <div id="header">
+                <Link href="/"><Image width="32" height="32" src="/assets/careo-temp-logo.png" alt="Careo Logo"></Image></Link>
+                {userData && userData.email ? <span>Welcome, <b>{userData.email.split('@')[0]}</b> <a href='' style={{fontSize: '0.8em'}} onClick={handleLogout}>Logout</a></span> : null}
+            </div>
+            {userData && userData.email ? <TabNavigation/> : null}
+        </>
     )
 }
--- pages/recommendation.js
+++ pages/recommendation.js
@@ -1,27 +1,54 @@
-import { Form, Alert, Button } from "react-bootstrap"
+import { Button } from "react-bootstrap"
 import { useRouter } from "next/router";
-import { useAtom } from "jotai";
-import {Col, Row } from "react-bootstrap"; 
-import ProductCard from "@/components/ProductCard";
-import { cartItemsAtom, userAtom } from "@/store";
-import TabNavigation from "@/components/TabNavigation";
-//import { isAuthenticated } from "@/lib/authenticate";
 
-//Going to use atoms to keep track of products selected for purchase, for now just hardcoding some products for testing
 export default function Recommendation(){
     const router = useRouter();
+
+    // Temporary placeholder data
+    let recs = [
+        {name: "Aveeno Moisturizer", price: 19.99, image: "https://via.placeholder.com/150"}, 
+        {name: "Sephora Skincare", price: 24.99, image: "https://via.placeholder.com/150"},
+        {name: "Aveeno Skincare", price: 89.99, image: "https://via.placeholder.com/150"},
+        {name: "ABC Hair Product", price: 19.99, image: "https://via.placeholder.com/150"},
+        {name: "XYZ Hair Product", price: 24.99, image: "https://via.placeholder.com/150"},
+        {name: "Aveeno Hair Product", price: 89.99, image: "https://via.placeholder.com/150"},
+        {name: "Aveeno Skincare", price: 19.99, image: "https://via.placeholder.com/150"},
+        {name: "Sephora Skincare", price: 24.99, image: "https://via.placeholder.com/150"},
+        {name: "Aveeno Skincare", price: 89.99, image: "https://via.placeholder.com/150"},
+        {name: "ABC Hair Product", price: 19.99, image: "https://via.placeholder.com/150"},
+        {name: "XYZ Hair Product", price: 24.99, image: "https://via.placeholder.com/150"}
+    ]
+
+    function selectRec(e){
+        console.log('clicked on ' + e.currentTarget)
+        let recs = document.querySelectorAll("#rec-table tr");
+        recs.forEach(rec => {
+            rec.classList.remove("rec-selected");
+        });
+        e.currentTarget.classList.add("rec-selected");
+    }
 
     return (
         <>
             <div style={{textAlign:"center"}} id="hero-text">
                 <h1>Recommendations</h1><br/>
-                <TabNavigation/>
             </div>
             <div>
-                <div className='centered' style={{paddingTop: '50px', paddingLeft: '30px'}}>
-                    <p>Product recommendations are based on Care Profile conditions</p>
-                </div>
-                <div className='centered' style={{paddingTop: '50px'}}>
+                <div className='centered' style={{flexDirection:"column"}}>
+                    <p>Product recommendations are based on Care Profile conditions</p>                    
+                    
+                    <div id="recommendation-box">
+                        <table id="rec-table">
+                            <tbody>
+                                {recs.map((rec, i) => (
+                                    <tr onClick={selectRec} key={i} className={(i == 0) ? "rec-selected" : null}>
+                                        <td><p>{rec.name}</p><p>${rec.price}</p></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                    <br/>
                     <Button variant="primary" className="centered" type="submit">Add to Cart</Button>
                 </div>
             </div>

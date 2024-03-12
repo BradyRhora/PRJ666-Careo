37a470553b7b7e571528f1a292e4e60066725a73
Brady Rhora <brady0423@gmail.com>
2024-03-12 16:35:57 -0400

    Add billing address functionality to payment page

--- pages/payment.js
+++ pages/payment.js
@@ -6,10 +6,13 @@ import { orderInfoAtom } from "@/store";
 import { use, useEffect } from "react";
 
 const paymentAtom = atom("creditCard");
+const billingAddressAtom = atom(true);
 
 export default function PlaceOrder() {
     const orderInfo = useAtomValue(orderInfoAtom);
+    const [billingAddressSame, setBillingAddressSame] = useAtom(billingAddressAtom);
     const [paymentMethod, setPaymentMethod] = useAtom(paymentAtom);
+
     const router = useRouter();
     
     if (!orderInfo || !orderInfo.firstName) { // If shipping info has been lost due to refresh or direct navigation
@@ -18,10 +21,15 @@ export default function PlaceOrder() {
         }
     }
 
+    function toggleBillingAddress(e) {
+        setBillingAddressSame(e.target.checked);
+    }
+
     function changePaymentMethod(e) {
         setPaymentMethod(e.target.name);
     }
 
+    // TODO: maybe replace 'Credit Card' and 'PayPal' with icons
     return(
 		<>
 		<div style={{textAlign:"center", paddingTop:"10px"}} id="hero-text">
@@ -31,17 +39,17 @@ export default function PlaceOrder() {
             <Form id="payment-info-form">
                 <Row className="mb-3">
                     <Form.Group as={Col} >
-                        <Form.Label>Payment Method</Form.Label>
-                        <div style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly'}}>
-                            <div class="form-check">
-                                <input onChange={changePaymentMethod} class="form-check-input" type="radio" name="creditCard" id="flexRadioDefault1" checked={paymentMethod==="creditCard"}></input>
-                                <label class="form-check-label" for="flexRadioDefault1">
+                        <p>Payment Method</p>
+                        <div className="inline-flex">
+                            <div className="form-check" style={{marginRight:'10px'}}>
+                                <input onChange={changePaymentMethod} className="form-check-input" type="radio" name="creditCard" id="payment-method-credit" checked={paymentMethod==="creditCard"}></input>
+                                <label className="form-check-label" for="payment-method-credit">
                                     Credit Card
                                 </label>
                             </div>
-                            <div class="form-check">
-                                <input onChange={changePaymentMethod} class="form-check-input" type="radio" name="paypal" id="flexRadioDefault2" checked={paymentMethod==="paypal"}></input>
-                                <label class="form-check-label" for="flexRadioDefault2">
+                            <div className="form-check">
+                                <input onChange={changePaymentMethod} className="form-check-input" type="radio" name="paypal" id="payment-method-paypal" checked={paymentMethod==="paypal"}></input>
+                                <label className="form-check-label" for="payment-method-paypal">
                                     PayPal
                                 </label>
                             </div>
@@ -49,17 +57,82 @@ export default function PlaceOrder() {
                     </Form.Group>
                 </Row>
                 {paymentMethod === "creditCard" && <>
+                <Row className="mb-3">
+                    <Form.Group className="inline-flex">
+                        <Form.Check checked={billingAddressSame} onChange={toggleBillingAddress} type="checkbox" id="same-billing-address" name="same-billing-address" value="same-billing-address" style={{marginRight:"10px"}}></Form.Check>
+                        <Form.Label>Billing address same as shipping address</Form.Label>
+                    </Form.Group>
+                </Row>
+                {billingAddressSame === false && <div id="billing-address-box">
+                    <Row className="mb-3">
+					<Form.Group as={Col} >
+						<Form.Label>First name</Form.Label>
+						<Form.Control type="text" placeholder="John" required></Form.Control>
+					</Form.Group>
+					<Form.Group as={Col}>
+						<Form.Label>Last name</Form.Label>
+						<Form.Control type="text" placeholder="Smith" required></Form.Control>
+					</Form.Group>
+				</Row>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>Address</Form.Label>
+						<Form.Control type="text" placeholder="1750 Finch Avenue East" required></Form.Control>
+					</Form.Group>
+				</Row>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>Apartment, Suite, etc. (optional) </Form.Label>
+						<Form.Control type="text" placeholder="Apt. 123"></Form.Control>
+					</Form.Group>
+				</Row>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>City</Form.Label>
+						<Form.Control type="text" placeholder="Toronto" required></Form.Control>
+					</Form.Group>
+					<Form.Group as={Col}>
+						<Form.Label>Province</Form.Label>
+						<Form.Select>
+							<option value="1">AB</option>
+							<option value="2">BC</option>
+							<option value="3">MB</option>
+							<option value="4">NB</option>
+							<option value="5">NL</option>
+							<option value="6">NS</option>
+							<option value="7">ON</option>
+							<option value="8">PE</option>
+							<option value="9">QC</option>
+							<option value="10">SK</option>
+						</Form.Select>
+					</Form.Group>
+				</Row>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>Country</Form.Label>
+						<Form.Select>
+							<option value="1">Canada</option>
+						</Form.Select>
+					</Form.Group>
+				</Row>
+				<Row className="mb-3">
+					<Form.Group as={Col}>
+						<Form.Label>Postal Code</Form.Label>
+						<Form.Control type="text" placeholder="M2J 2X5"></Form.Control>
+					</Form.Group>
+				</Row>
+                </div>}
 				<Row className="mb-3">
 					<Form.Group as={Col} >
 						<Form.Label>Card Number</Form.Label>
 						<Form.Control type="text" placeholder="123412340987" required></Form.Control>
 					</Form.Group>
+				</Row>
+				<Row className="mb-3">                    
 					<Form.Group as={Col}>
 						<Form.Label>Expiry Date</Form.Label>
 						<Form.Control type="text" placeholder="01/23" required></Form.Control>
 					</Form.Group>
-				</Row>
-				<Row className="mb-3">
 					<Form.Group as={Col}>
 						<Form.Label>Security Code</Form.Label>
 						<Form.Control type="text" placeholder="123" required></Form.Control>
@@ -69,7 +142,7 @@ export default function PlaceOrder() {
 
                 {paymentMethod === "paypal" && <>
                 <Row className="mb-3">
-                    <h4 style={{border:'2px solid red', textAlign:'center', padding:'20px'}}>PayPal payment not yet integrated</h4>
+                    <h4 style={{border:'2px solid red', borderRadius: '5px',textAlign:'center', padding:'20px'}}>PayPal payment not yet integrated</h4>
                 </Row>
                 </>}
 

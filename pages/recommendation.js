a2c17c45ec1c82602adc09264731a4fe04fedb1e
Aditya Rahman <arahman5395@gmail.com>
2024-03-12 06:26:23 -0400

    Features: Save list, load list, clear list, reset user care profile, and alert system for list management

--- pages/recommendation.js
+++ pages/recommendation.js
@@ -1,15 +1,18 @@
-import { Button } from "react-bootstrap"
+import { Button, Card, Modal } from "react-bootstrap"
 import { useRouter } from "next/router";
 import { atom, useAtom } from "jotai";
 import Image from 'next/image';
 import { useEffect, useMemo, useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import { addSavedLists } from "@/lib/addSavedList";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default function Recommendation(){
     const router = useRouter();  
     const [recs, setRecs] = useState();
-    const [selectedProduct, setSelectedProduct] = useState({});
-    
+    const [selectedProduct, setSelectedProduct] = useState({});    
+
     useEffect(() => {
         if (!recs) {
             fetch("/api/recommendations/getrecommendations").then(res => res.json()).then(data => {
@@ -28,6 +31,65 @@ export default function Recommendation(){
         });
         e.currentTarget.classList.add("rec-selected");
         setSelectedProduct(recs[e.currentTarget.rowIndex]);
+    }
+
+    const saveCurrList = async () => {
+        if(recs && recs[0]) {
+            addSavedLists(recs);       
+
+            confirmAlert({
+                title: 'Current recommendation list saved!',
+                buttons: [
+                    {
+                        label: 'Ok',
+                    },
+                ]
+            });
+        }
+    }
+
+    const loadSavedList = () => {      
+        let viewList = fetch("/api/recommendations/getsavedlist").then(res => res.json()).then(data => {
+            if(data.savedLists != undefined){
+                if(data.savedLists.length > 0) {
+                    const reccLists = [];
+                    for (let i = 0; i < data.savedLists.length; i++){
+                        reccLists.push("Saved list " + (i + 1));
+
+                    }
+
+                    confirmAlert({
+                        customUI: ({ onClose }) => {
+                            return (
+                                <div className='custom-ui'>
+                                    <h4>Please select a saved list to load:</h4>
+                                    <table id="rec-table">
+                                        <tbody>
+                                            {reccLists.map((rec, i) => (
+                                                <tr onClick={() => {setRecs(data.savedLists[i].savedRecc); onClose();}} key={i}>
+                                                    <td><p>Saved list {i + 1}</p></td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            );
+                        }
+                    });
+                }
+            }
+            else{
+                confirmAlert({
+                    title: 'No saved list found!',
+                    message: 'Please save recommendation list to load them.',
+                    buttons: [
+                        {
+                            label: 'Ok',
+                        },
+                    ]
+                });
+            }
+        });
     }
 
     /* TODO: The selected product box (rec-selected-box) still needs a section for ingredients and will have to be modified when the actual recommendation system is implemented */
@@ -55,11 +117,12 @@ export default function Recommendation(){
                     </div>
                     <br/>
                     <div className="button-menu">
-                        <Button variant="primary" className="centered">Save List</Button>
-                        <Button variant="secondary" className="centered">Load List</Button>
+                        <Button variant="primary" className="centered" onClick={saveCurrList}>Save List</Button>
+                        <Button variant="secondary" className="centered" onClick={loadSavedList}>Load List</Button>
                     </div>
                     <br/>
-
+                    <div>
+                    </div>
                     <div id="rec-selected-box">
                         { (selectedProduct && selectedProduct.length > 0) ?
                             <Row>

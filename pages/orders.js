42d2dc3c8f95bfcc8cde7ef5fcd37a3ddb38b6d3
Brady Rhora <brady0423@gmail.com>
2024-03-25 18:08:21 -0400

    Refactor order display and styling

--- pages/orders.js
+++ pages/orders.js
@@ -20,8 +20,12 @@ const OrdersPage = () => {
             data = data.sort((a, b) => new Date(b.order_date) - new Date(a.order_date));
             data.forEach((order) => {
                 order.order_date = new Date(order.order_date).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'});
+                order.status = order.status.charAt(0).toUpperCase() + order.status.slice(1);
                 if (order.payment_method === 'creditCard') order.payment_method = 'Credit Card';
                 else if (order.payment_method === 'paypal') order.payment_method = 'PayPal';
+                order.items.forEach((item) => {
+                    if (item.quantity > 1) item.productId.name += " (x" + item.quantity + ")";
+                });
             });
             setOrders(data)
         });
@@ -32,7 +36,7 @@ const OrdersPage = () => {
             <h3>Order History</h3>
         </div>
         <div style={{flowDirection:"column"}}className="centered">
-            <ul>
+            <ul id="order-history-list">
                 {orders && orders?.length > 0 && orders.map((order) => (
                     <div key={order._id} class="order-box">
                         <p><b>Order ID:</b> {order._id}</p>
@@ -41,9 +45,11 @@ const OrdersPage = () => {
                         <p><b>Shipping Address:</b> {order.shipping_address}</p>
                         <p><b>Billing Address:</b> {order.billing_address}</p>
                         <p><b>Payment Method:</b> {order.payment_method}</p>
+                        <p><b>Status:</b> {order.status}</p>
+                        <p><b>Items:</b></p>
                         <ul>
                             {order.items.map((item) => (
-                                <li key={item.productId._id}>{item.quantity}x {item.productId.name}</li>
+                                <li key={item.productId._id}>{item.productId.name}</li>
                             ))}
                         </ul>
                     </div>

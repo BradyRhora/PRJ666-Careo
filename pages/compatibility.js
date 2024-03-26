5b1e1f26ffc2be71749c12f69b20e99d397fb3bb
Aditya Rahman <arahman5395@gmail.com>
2024-03-26 00:48:26 -0400

    compatibility page filtered search

--- pages/compatibility.js
+++ pages/compatibility.js
@@ -1,27 +1,79 @@
-
+import { useEffect, useState } from "react";
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default function Recommendation(){
+    const [allproducts, setAllProducts] = useState();
+	const [searchItem, setSearchItem] = useState();
+	const [filteredProducts, setFilteredProducts] = useState();
+
+    useEffect(() => {
+        if (!allproducts) {
+            fetch("/api/product/getallproducts").then(res => res.json()).then(data => {
+                setAllProducts(data);
+
+            });
+        }
+
+    }, [allproducts]);
+
+	const handleInputChange = (e) => {
+		const searchProduct = e.target.value;
+		setSearchItem(searchProduct);
+
+		if (searchProduct == ""){
+			setFilteredProducts([]);
+
+		}
+		else{
+			const filteredItems = allproducts.filter((p) =>
+				p.name.toLowerCase().startsWith(searchProduct.toLowerCase())
+			);
+
+			setFilteredProducts(filteredItems);
+		}
+	}
+
+	//onSearch will hand the request to get compatible products by sending either id or the name, need to decide later
+	const onSearch = (e) => {
+
+	}
+
+	const handleProductSelect = (productName) => {
+		setSearchItem(productName);
+		setFilteredProducts([]);
+	
+	}
+
 	return(
 		<>
-			<h2 id="hero-text">Product Compatibility (Coming Soon)</h2>
+			<h2 id="hero-text">Product Compatibility</h2>
 			<h5 style={{textAlign:"center"}}>Search a product name to check compatibility with other products</h5>
-			<div className="centered">
-				<select style={{margin:"1rem", width:"50%", padding:"0.5rem"}}>
-					<option value="1">Product A</option>
-					<option value="2">Product AA</option>
-					<option value="3">Product AAA</option>
-					<option value="4">Product B</option>
-					<option value="5">Product BB</option>
-					<option value="6">Product BBB</option>
-					<option value="7">Product C</option>
-					<option value="8">Product CC</option>
-					<option value="9">Product CCC</option>
-					<option value="10">Product D</option>
-				</select>	
+			<br></br>
+			<div>
+				<div className="centered">
+					<input
+						style={{width: "50%"}}
+						type="text"
+						value={searchItem}
+						onChange={handleInputChange}	
+						placeholder="Enter product name"
+					/>
+					<button onClick={() => onSearch(searchItem)}>Search</button>
+				</div>
+				<div className="centered">
+					<table id="rec-table" style={{width:"50%"}}>
+						<tbody>
+							{filteredProducts && filteredProducts.map(p => 
+								<tr onClick={() => handleProductSelect(p.name)} key={p._id}>
+									<td>{p.name}</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+				</div>
 			</div>
-
 			<div className="centered">
-				<table id="rec-table" style={{width:"50%", margin:"1rem"}}>
+				<table id="rec-table" style={{width:"60%", margin:"1rem"}}>
 					<tbody>
 						<tr style={{background:'white'}}>
 							<th className="centered">Compatible Products</th>
